41930d0ff82efc19e27f9af3611d13534b1e0937
B.Viktor <B.Viktor@sulid.hu>
2023-02-25 11:59:01 +0100

    commit #1

--- noteapp/MainWindow.xaml.cs
+++ noteapp/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,49 +16,59 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace noteapp {
+namespace noteapp
+{
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow:Window {
-        public MainWindow() {
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
             InitializeComponent();
             filenames();
             dt1.ItemsSource = upload1;
-            }
+        }
         public string[] upload1;
-        public void filenames() {
+        public void filenames()
+        {
             string[] Names = Directory.GetFiles("Notes/");
             upload1 = new string[Names.Length];
             int counter = 0;
-            foreach(string i in Names) {
+            foreach (string i in Names)
+            {
                 string[] tmp0 = i.Split("/");
                 string[] tmp1 = tmp0[1].Split(".");
                 upload1[counter] = tmp1[0];
                 counter++;
-                }
             }
+        }
 
-        private void TextBox_PreviewMouseDown(object sender,MouseButtonEventArgs e) {
+        private void TextBox_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
             tb1.Text = "";
-            }
+        }
 
-        private void Savefunc(object sender,RoutedEventArgs e) {
+        private void Savefunc(object sender, RoutedEventArgs e)
+        {
             StreamWriter sw = new StreamWriter(filename.Text);
             sw.WriteLine(tb1.Text);
             sw.Close();
-            }
+        }
 
-        private void Openfunc(object sender,RoutedEventArgs e) {
+        private void Openfunc(object sender, RoutedEventArgs e)
+        {
 
-            }
+        }
 
-        private void Deletefunc(object sender,RoutedEventArgs e) {
+        private void Deletefunc(object sender, RoutedEventArgs e)
+        {
 
-            }
+        }
 
-        private void Newfunc(object sender,RoutedEventArgs e) {
+        private void Newfunc(object sender, RoutedEventArgs e)
+        {
 
-            }
         }
     }
+}
